5283919c04865e8967b7a70cf5253c5d3670fa5f
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-04-02 22:34:15 +0300

    Getting JSON from our URL

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api()
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Антон - крип'
+class Main(Resource):
+    def get(self):
+        return {"info": "Some info", "num": 56}
 
+
+api.add_resource(Main, "/api/main")
+api.init_app(app)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=3000, host="127.0.0.1")
